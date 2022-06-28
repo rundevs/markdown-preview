c53a1221a04ee90be0082ca074129a8f75f71174
Luis Gabriel Janco <janco7249@gmail.com>
2022-06-28 12:44:06 -0400

    fix: add document fragment in rootNode

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import 'styles/index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const rootNode = document.getElementById('root') ?? document.createDocumentFragment()
+
+ReactDOM.createRoot(rootNode).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
