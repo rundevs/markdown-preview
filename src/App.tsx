cb0ea49b3c1a5e5775bd0e684993a5078d8b6eab
Luis Gabriel Janco <janco7249@gmail.com>
2022-07-01 10:09:21 -0400

    feat: add editor wrapper component

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,12 @@
-import { useState } from 'react'
-import electron from '/electron.png'
-import react from '/react.svg'
-import vite from '/vite.svg'
-import styles from 'styles/app.module.scss'
+import React from 'react'
+import Editor from './components/Editor'
+import styles from './assets/styles/app.module.css'
 
 const App: React.FC = () => {
-  const [count, setCount] = useState(0)
 
   return (
     <div className={styles.app}>
-      <header className={styles.appHeader}>
-        <div className={styles.logos}>
-          <div className={styles.imgBox}>
-            <img
-              src={electron}
-              style={{ height: '24vw' }}
-              className={styles.appLogo}
-              alt="electron"
-            />
-          </div>
-          <div className={styles.imgBox}>
-            <img src={vite} style={{ height: '19vw' }} alt="vite" />
-          </div>
-          <div className={styles.imgBox}>
-            <img
-              src={react}
-              style={{ maxWidth: '100%' }}
-              className={styles.appLogo}
-              alt="logo"
-            />
-          </div>
-        </div>
-        <p>Develop in React typescript on the Electron framework with the vite engine</p>
-        <p>
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-      </header>
+      <Editor />
     </div>
   )
 }
---
+++ src/components/Editor/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import style from './editor.module.css'
+
+const Editor: React.FC = () => {
+  return <div className={style.editorWrapper}>Editor</div>
+}
+
+export default Editor
