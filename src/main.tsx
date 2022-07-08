43874dbd9f31035941c8cf24036eb2db829cadfe
Luis Gabriel Janco <janco7249@gmail.com>
2022-07-07 22:44:32 -0400

    feat: add Code editor of root app

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import App from './App'
 import 'styles/index.css'
 
 const rootNode = document.getElementById('root') ?? document.createDocumentFragment()
+const root = createRoot(rootNode)
 
-ReactDOM.createRoot(rootNode).render(
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
