ac1c5b2ee9504942112cb88ec095b7a6a68efd78
Luis Gabriel Janco <janco7249@gmail.com>
2022-07-09 16:09:50 -0400

    refactor: changes initial code of markdown

---
+++ packages/renderer/src/utils/example.ts
@@ -0,0 +1,25 @@
+export const exampleMarkdown = `# Welcome to Markdown Preview
+## Basic Syntax
+### Heading 3
+
+- List Item 1
+- List Item 2
+
+1. Ordered list item 1
+2. Ordered list item 2
+
+---
+\`\`\`js
+const message = "Hello "
+function sayHello(name) {
+  return message + name
+}
+console.log(sayHello('Bob')) // Hello Bob
+\`\`\`
+
+> Quote,
+> Hello world of quote
+
+- [ ] Task item todo
+- [x] Task list item checkend
+`
