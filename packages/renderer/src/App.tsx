a7649e87dc8a0637d3ac205bd31ec1cf2a1174e9
Luis Gabriel Janco <janco7249@gmail.com>
2022-07-10 19:04:57 -0400

    refactor: add conditional if is linux to change background-color

--- packages/renderer/src/App.tsx
+++ packages/renderer/src/App.tsx
@@ -1,13 +1,30 @@
-import React, { useCallback, useState } from 'react'
+import os from 'os'
+import React, { useCallback, useEffect, useState } from 'react'
 import CodeEditor from './components/Editor/CodeEditor'
 import style from './assets/styles/app.module.css'
 import Preview from './components/Preview/Preview'
 import { exampleMarkdown } from './utils/example'
 import NavBar from './components/Navigation/NavBar'
 
-const App = (): JSX.Element => {
+const changeStyle = () => {
+  document.body.style.backgroundColor = 'transparent'
+}
+
+export const isLinux = os.platform() === 'linux'
+
+const App: React.FC = (): JSX.Element => {
   const [doc, setDoc] = useState<string>(exampleMarkdown)
-  const handleDocChange = useCallback((newDoc: any) => {
+
+  useEffect(() => {
+    if (isLinux) {
+      changeStyle()
+    }
+    return () => {
+      // clean effect
+    }
+  }, [])
+
+  const handleDocChange = useCallback((newDoc: string): void => {
     setDoc(newDoc)
   }, [])
 
