d6c9b8cdcd46270af25d7cb2ca501b519f077b03
Luis Gabriel Janco <janco7249@gmail.com>
2022-07-11 18:12:58 -0400

    feat: add handlers action of title bar

--- packages/renderer/src/components/Navigation/NavBar.tsx
+++ packages/renderer/src/components/Navigation/NavBar.tsx
@@ -1,28 +1,41 @@
 import React from 'react'
 import style from './navigation.module.css'
 import viteLogo from '/vite.svg'
-import { isLinux } from '../../App'
 import { Close, Dash, MaximizeLeft, MaximizeRight } from '../../assets/icons'
+import { isLinux } from '../../App'
+const { ipcRenderer } = window.require('electron')
+const ipc = ipcRenderer
 
+/**
+ * @description you should ask me if you can close the window
+ * @todo ask me if you can close the window
+ * @todo replace window.confirm for custom modal
+ * */
+const handleClose = (): void => {
+  // const closed = confirm('Are you sure you want to close the app?')
+  ipc.send('closeApp')
+}
+/** @description you should minimize window */
+const handleMinimize = (): void => {
+  ipc.send('minimizeApp')
+}
+/** @description you should maximize or restore window */
+const handleMaximize = (): void => {
+  ipc.send('maximizeRestoreApp')
+}
 const NavBar: React.FC = (): JSX.Element => {
-  /** @description you should ask me if you can close the window */
-  const handleClose = (): void => {
-    const closed = confirm('Are you sure you want to close the app?')
-    closed && window.close()
-  }
-
   return (
     <nav className={style.navigation}>
       <div className={style.navLeft}>
         {isLinux ? (
           <div className={style.trafficLight}>
-            <div className={style.red}>
+            <div onClick={handleClose} className={style.red}>
               <Close />
             </div>
-            <div className={style.orange}>
+            <div onClick={handleMinimize} className={style.orange}>
               <Dash />
             </div>
-            <div className={style.green}>
+            <div onClick={handleMaximize} className={style.green}>
               <MaximizeLeft />
               <MaximizeRight />
             </div>
